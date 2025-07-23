516af00cbfb1b98765c4a2456e95fff26d7eea52
Jürgen Czerny <juergen.czerny@sovanta.com>
2025-07-23 10:00:14 +0200

    formatting changes

--- .config/nvim/lua/plugins/zellij.lua
+++ .config/nvim/lua/plugins/zellij.lua
@@ -7,5 +7,7 @@ return {
         -- Options:
         -- vim.g.zelli_navigator_move_focus_or_tab = 1
         -- vim.g.zellij_navigator_no_default_mappings = 1
+        -- nnoremap <leader>zjf :ZellijNewPane<CR>
+        vim.keymap.set("n", "<M-f>", ":ZellijNewPane<CR>", { desc = "Open new Zellij floating Pane" })
     end,
 }
